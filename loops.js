1e60f31ab47be8ff09f3ffb5a2f421bc41ca48cc
E.B <edenbenbow@yahoo.com>
2019-07-24 03:16:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -12,11 +12,4 @@ function whileLoop(n) {
   while (n > 0) {console.log(--n)}
   return "done"}
 
-  function doWhileLoop(array) {
-function maybeTrue() {
-  return Math.random() >= 0.5
-}
-do {array.pop};
-  } while (array.length > 0 && maybeTrue());
-  return array;
-}
+
