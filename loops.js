e9116970d4bb6004d963fb8b646d54e40f450f9d
E.B <edenbenbow@yahoo.com>
2019-07-24 03:09:53 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -9,9 +9,5 @@ function forLoop(array) {
 }
 
 function whileLoop() {
-  let countdown = n;
-
-  while (countdown > 0) {
-    console.log(--countdown);
-  }
-}
+  while (n > 0) {console.log(--n)}
+  return "done"}
