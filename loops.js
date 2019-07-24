1a583530e2aa6538fda26353058e4252bf1569bc
E.B <edenbenbow@yahoo.com>
2019-07-24 03:15:30 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -11,3 +11,12 @@ function forLoop(array) {
 function whileLoop(n) {
   while (n > 0) {console.log(--n)}
   return "done"}
+
+  function doWhileLoop(array) {
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+do {array.pop};   
+  } while (array.length > 0 && maybeTrue());
+  return array;
+}
